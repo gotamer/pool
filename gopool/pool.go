f8efdc66c483cfb535e7ad21936db07e00fd5013
Brian Johnson <bbjohnson@gmail.com>
2013-01-18 16:51:58 -0700

    some testing

--- gopool/pool.go
+++ gopool/pool.go
@@ -8,53 +8,60 @@ type Resource struct {
 }
 
 type pool struct {
+	count int
 	max int
-//	min int // TODO support min
-	freeResources chan Resource
-	inUseResources chan Resource
-//	maxIdleTime int // TODO implement?
-	create func() interface{}
-	destroy func()
+//	min int // TODO support min?
+	resources chan Resource
+//	maxIdleTime int // TODO support an idle time?
+	create func() (interface{})
+	destroy func(Resource)
 }
 
-func Initialize(max int, create func() interface{}, destroy func()) (pool) {
+func Initialize(max int, create func() interface{}, destroy func(Resource)) (pool) {
 	p := pool{}
+	p.count = 0
 	p.create = create
 	p.max = max
 	p.destroy = destroy
-	p.freeResources = make(chan Resource)
-	p.inUseResources = make(chan Resource)
+	p.resources = make(chan Resource, 10)
 	return p
 }
 
 func (p *pool) Drain() {
-
+	println("draining pool")
+	println(len(p.resources))
+	for len(p.resources) > 0 {
+		resource := <-p.resources
+		p.destroy(resource)
+	}
 }
 
 func (p *pool) Acquire() (error, Resource) {
 	select {
-	case resource := <-p.freeResources:
+	case resource := <-p.resources:
 		// resource available
-		p.inUseResources <- resource
+		println("Obtained free resource")
 		return nil, resource
 	default:
 		// resource not immediately available
-		if len(p.freeResources) + len(p.inUseResources) < p.max - 1 {
-			// if we have less resources than max 
-			// attempt to create a new resource
+		if p.count < p.max {
+			// if we have less resources than max attempt to create a new resource
 			resource := Resource{}
 			val := p.create()
+			resource.Payload = val
 			if (val != nil) {
-				p.inUseResources <- val.(Resource)
+				p.count++
 				resource.Payload = val
+				println("Resource created and returned")
 				return nil, resource
 			} else {
+				println("Could not create new resource")
 				return errors.New("Could not create new resource"), Resource{}
 			}
 		} else {
 			// wait for a resource to become available
-			resource := <-p.freeResources
-			p.inUseResources <- resource
+			resource := <-p.resources
+			println("Previously created resource returned")
 			return nil, resource
 		}
 	}
@@ -62,4 +69,6 @@ func (p *pool) Acquire() (error, Resource) {
 }
 
 func (p *pool) Release(resource Resource) {
+	go func(){ p.resources <- resource; println("put back"); println(len(p.resources)) }()
+	println("Resource returned to pool")
 }
--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
+import "time"
+
 import "./gopool"
 
 func main() {
+	create := func() (interface{}) {
+		time.Sleep(time.Millisecond * 100)
+		return "hello world"
+	}
+	destroy := func(gopool.Resource) {
+		println("bye bye world")
+	}
+
+	myPool := gopool.Initialize(10, create, destroy)
+
+	err, resource1 := myPool.Acquire()
+	if (err == nil) {
+		msg := resource1.Payload
+		println(msg.(string))
+	} else {
+		println(err)
+	}
+	go func() { time.Sleep(time.Millisecond * 1000); myPool.Release(resource1) }()
+
+	err, resource2 := myPool.Acquire()
+	if (err == nil) {
+		msg := resource2.Payload
+		println(msg.(string))
+	} else {
+		println(err)
+	}
+	go func() { time.Sleep(time.Millisecond * 1000); myPool.Release(resource2) }()
+
+	err, resource3 := myPool.Acquire()
+	if (err == nil) {
+		msg := resource3.Payload
+		println(msg.(string))
+	} else {
+		println(err)
+	}
+
+	err, resource4 := myPool.Acquire()
+	if (err == nil) {
+		msg := resource4.Payload
+		println(msg.(string))
+	} else {
+		println(err)
+	}
+	myPool.Release(resource3)
+	myPool.Release(resource4)
+
+	time.Sleep(time.Millisecond * 1000)
+
+	myPool.Drain()
+
+	time.Sleep(time.Millisecond * 1000)
 
 }
+
+
+
+
